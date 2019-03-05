90fa8a7955b76e4157c7d0a579b39d567999e368
sugan84 <sugandran@gmail.com>
2019-03-05 17:17:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello_world():
   #return 'Hey its Python Web application!'
   return 'Sugans test python service!'
+
+@app.route('/Home')
+def home():
+  return 'Home Page!'
   
 
 if __name__ == '__main__':
